c8a92383509629fe5c4e8e8ffa6caae8299bc9c5
wilkinong1 <wilkin.ong@gmail.com>
2023-12-21 21:07:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from datetime import datetime, timedelta
 from streamlit_shap import st_shap
 import pandas as pd
 
-
 st.set_page_config(page_title='Capstone Project App', page_icon=None, layout="wide", initial_sidebar_state='collapsed')
 st.set_option('deprecation.showPyplotGlobalUse', False)
 

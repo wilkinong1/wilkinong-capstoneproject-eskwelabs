92edfa6a98d894639c83509739a937b08a24f2e7
wilkinong1 <wilkin.ong@gmail.com>
2023-12-26 22:15:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from streamlit_shap import st_shap
 import pandas as pd
 
 
-st.set_page_config(page_title='Capstone Project App', page_icon=None, layout="wide", initial_sidebar_state='collapsed')
+
+st.set_page_config(page_title='LP Companion', page_icon=None, layout="wide", initial_sidebar_state='collapsed')
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 if 'filtered_data' not in st.session_state:
